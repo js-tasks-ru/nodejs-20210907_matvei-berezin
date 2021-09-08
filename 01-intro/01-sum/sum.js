0b4e06dec7663273bd3081b6525b5d4ee48f4ce9
MathewBr <matvei_berezin@mail.ru>
2021-09-08 20:01:41 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
   /* ваш код */
+  if (typeof a == 'number' && typeof b == 'number') {
+    return a + b;
+  } else {
+    return new TypeError('аргумент не число');
+  }
 }
+console.log(sum(2, '3'));
 
 module.exports = sum;
