2c7d1332c5a65c0ef8b98093f7beabc5bbcc58e1
Matvey Berezin <matvei_berezin@mail.ru>
2021-10-13 00:52:08 +0300

    add solution for 6-module 2-task

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,53 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  // const findCategories = [
+  //   {
+  //     id: '5d208e631866a7366d831ff1',
+  //     title: 'Category1',
+  //     subcategories: [{
+  //       id: '5d208e631866a7366d83_1_1',
+  //       title: 'Subcategory1.1',
+  //     }, {
+  //       id: '5d208e631866a7366d83_1_2',
+  //       title: 'Subcategory1.2',
+  //     }],
+  //   },
+
+  //   {
+  //     id: '5d208e631866a7366d831ff2',
+  //     title: 'Category2',
+  //     subcategories: [{
+  //       id: '5d208e631866a7366d83_2_1',
+  //       title: 'Subcategory2.1',
+  //     }],
+  //   },
+  // ];
+  const categories = await Category.find({});
+
+  console.log(categories);
+
+  ctx.body = {
+    categories: [...categories].map((category) => ({
+      id: category._id,
+      title: category.title,
+      subcategories: category.subcategories.map((sub) => ({
+        id: sub._id,
+        title: sub.title,
+      })),
+    })),
+  };
 };
+
+// {
+//   categories: [
+//     {
+//     id: '5d208e631866a7366d831ffc',
+//     title: 'Category1',
+//     subcategories: [{
+//       id: '5d208e631866a7366d831ffd',
+//       title: 'Subcategory1'
+//     }]
+//   }
+//   ]
+// }
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,71 @@
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
+  if (!subcategory) {
+    ctx.body = {};
+    return next();
+  }
 
-  if (!subcategory) return next();
+  const productsBySubcategory = await Product.find({subcategory: subcategory});
 
-  ctx.body = {};
+  ctx.body = {
+    products: productsBySubcategory.map((product) => ({
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      subcategory: product.subcategory,
+      price: product.price,
+      description: product.description,
+    }),
+    ),
+  };
 };
-
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productList = await Product.find({});
+  ctx.body = {
+    products: productList.map((product) => ({
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      subcategory: product.subcategory,
+      price: product.price,
+      description: product.description,
+    }),
+    ),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!ObjectID.isValid(ctx.params.id)) {
+    ctx.status = 400;
+    ctx.body = 'не валидный Id';
+    return next();
+  };
+
+  const product = await Product.findById(ctx.params.id);
+
+  if (!product) {
+    ctx.status = 404;
+    ctx.body = 'такого товара нет';
+    return next();
+  };
+
+  ctx.body = {
+    product: {
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      subcategory: product.subcategory,
+      price: product.price,
+      description: product.description,
+    },
+  };
 };
+
 
