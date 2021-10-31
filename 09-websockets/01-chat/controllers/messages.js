5033a741089860ac235010686b3ad5dfe8c896eb
Matvey Berezin <matvei_berezin@mail.ru>
2021-10-31 03:33:42 +0300

    add solution for 9-module 1-task

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,15 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  const messageList = await Message.find().limit(1);
+
+  ctx.body = {
+    messages: messageList.map((message) => ({
+      id: message.id,
+      date: message.date,
+      text: message.text,
+      user: message.user,
+    }),
+    ),
+  };
 };
